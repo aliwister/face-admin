935c677b2dccf472c0acbbde0712539613562308
Ali Allawati <ali@badals.com>
2020-04-19 00:29:19 +0400

    allow longer authcodes

--- src/containers/Orders/components/RefundFormDialog.tsx
+++ src/containers/Orders/components/RefundFormDialog.tsx
@@ -40,7 +40,7 @@ export const RefundFormDialog = ({open, onSubmit, onClose, payment}) =>{
       <DialogContent>
       <TextField variant="filled" size="small" placeholder="Amount" name="amount" className={classes.textBox} inputRef={register({required: true, max: -.1, min:-9999, maxLength: 5})} />
       <br/>
-      <TextField variant="filled" size="small" type="text" placeholder="Auth Code" name="authCode" value={payment.authCode} className={classes.textBox} inputRef={register({required: true, maxLength: 6})} />
+      <TextField variant="filled" size="small" type="text" placeholder="Auth Code" name="authCode" value={payment.authCode} className={classes.textBox} inputRef={register({required: true, maxLength: 10})} />
       <br/>
         <FormControl variant="filled" size="small" className={classes.formControl}>
           <Select native name="paymentMethod" inputRef={register({ required: true })}>
